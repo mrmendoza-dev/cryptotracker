ca6ec593d6d44621aadda10c9d764e171fa08d1d
Mendoza <smendoza171@gmail.com>
2023-05-07 13:11:35 -0700

    webmanifest dark mode test

--- src/components/DarkMode/DarkMode.tsx
+++ src/components/DarkMode/DarkMode.tsx
@@ -15,9 +15,6 @@ export default function DarkMode() {
     font: "#111827",
     bg: "#ffffff",
     fontAccent: "#6B7280",
-    contrast: "#ad1c11",
-    accent1: "#bfc0c0",
-    accent2: "#4f5d75",
     shadow: "#999",
     row: "#F9FAFB",
     hr: "#F3F4F6",
@@ -28,9 +25,6 @@ export default function DarkMode() {
     font: "#EEEEEE",
     bg: "#121212",
     fontAccent: "#A0A0A0",
-    contrast: "#641ddf",
-    accent1: "#2f5270",
-    accent2: "#1e375a",
     shadow: "#333",
     row: "#1E1E1E",
     hr: "#2F2F2F",
@@ -41,12 +35,21 @@ export default function DarkMode() {
     root.style.setProperty("--clr-font", theme.font);
     root.style.setProperty("--clr-fontAccent", theme.fontAccent);
     root.style.setProperty("--clr-bg", theme.bg);
-    root.style.setProperty("--clr-contrast", theme.contrast);
-    root.style.setProperty("--clr-accent1", theme.accent1);
-    root.style.setProperty("--clr-accent2", theme.accent2);
     root.style.setProperty("--clr-shadow", theme.shadow);
     root.style.setProperty("--clr-row", theme.row);
     root.style.setProperty("--clr-hr", theme.hr);
+    // Set the theme_color and background_color properties in the webmanifest
+    const themeColorMeta = document.querySelector('meta[name="theme-color"]');
+    if (themeColorMeta) {
+      themeColorMeta.setAttribute("content", theme.bg);
+    }
+
+    const backgroundColorMeta = document.querySelector(
+      'meta[name="background-color"]'
+    );
+    if (backgroundColorMeta) {
+      backgroundColorMeta.setAttribute("content", theme.bg);
+    }
   }
 
   function setTheme() {
